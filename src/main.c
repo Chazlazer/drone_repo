fbd96f885891869bada6ca7bf4900b5a482c205e
Chazlazer <88365067+Chazlazer@users.noreply.github.com>
2024-05-05 15:43:21 -0700

    Update main.c
    Added mpu-dmp sensor code

--- src/main.c
+++ src/main.c
@@ -6,8 +6,16 @@
  */
 #include <stdio.h>
 #include <robotcontrol.h>
-void on_pause_press();
-void on_pause_release();
+
+#define I2C_BUS 2
+#define GPIO_INT_PIN_CHIP 3
+#define GPIO_INT_PIN_PIN  21
+
+static void __attitude_controller(void);
+
+// Global Variables
+static rc_mpu_data_t mpu_data;
+
 
 int main()
 {
@@ -18,19 +26,30 @@ int main()
                 fprintf(stderr,"ERROR: failed to start signal handler\n");
                 return -1;
         }
-        // initialize pause button
-        if(rc_button_init(RC_BTN_PIN_PAUSE, RC_BTN_POLARITY_NORM_HIGH,
-                                                RC_BTN_DEBOUNCE_DEFAULT_US)){
-                fprintf(stderr,"ERROR: failed to initialize pause button\n");
-                return -1;
-        }
-        // Assign functions to be called when button events occur
-        rc_button_set_callbacks(RC_BTN_PIN_PAUSE,on_pause_press,on_pause_release);
 
         rc_make_pid_file();
-        printf("\nPress and release pause button to turn green LED on and off\n");
-        printf("hold pause button down for 2 seconds to exit\n");
         // Keep looping until state changes to EXITING
+
+        rc_mpu_config_t mpu_config = rc_mpu_default_config();
+        mpu_config.dmp_sample_rate = 200;
+        mpu_config.orient = ORIENTATION_Y_UP;
+        mpu_config.enable_magnetometer = 1;
+        // if gyro isn't calibrated, run the calibration routine
+        if(!rc_mpu_is_gyro_calibrated()){
+                printf("Gyro not calibrated, automatically starting calibration routine\n");
+                printf("Let your MiP sit still on a firm surface\n");
+                rc_mpu_calibrate_gyro_routine(mpu_config);
+        }
+
+        // start mpu
+        if(rc_mpu_initialize_dmp(&mpu_data, mpu_config)){
+                fprintf(stderr,"ERROR: can't talk to IMU, all hope is lost\n");
+                rc_led_blink(RC_LED_RED, 5, 5);
+                return -1;
+        }
+
+        rc_mpu_set_dmp_callback(&__attitude_controller);
+
         rc_set_state(RUNNING);
         while(rc_get_state()!=EXITING){
                 // do things based on the state
@@ -53,30 +72,9 @@ int main()
         rc_remove_pid_file();   // remove pid file LAST
         return 0;
 }
-/**
- * Make the Pause button toggle between paused and running states.
- */
-void on_pause_release()
-{
-        if(rc_get_state()==RUNNING)     rc_set_state(PAUSED);
-        else if(rc_get_state()==PAUSED) rc_set_state(RUNNING);
-        return;
-}
-/**
-* If the user holds the pause button for 2 seconds, set state to EXITING which
-* triggers the rest of the program to exit cleanly.
-**/
-void on_pause_press()
-{
-        int i;
-        const int samples = 100; // check for release 100 times in this period
-        const int us_wait = 2000000; // 2 seconds
-        // now keep checking to see if the button is still held down
-        for(i=0;i<samples;i++){
-                rc_usleep(us_wait/samples);
-                if(rc_button_get_state(RC_BTN_PIN_PAUSE)==RC_BTN_STATE_RELEASED) return;
-        }
-        printf("long press detected, shutting down\n");
-        rc_set_state(EXITING);
-        return;
+
+static void __attitude_controller(void){
+    printf("%f",mpu_data.dmp_TaitBryan[TB_PITCH_X]*RAD_TO_DEG);
+    printf(" | %f",mpu_data.dmp_TaitBryan[TB_ROLL_Y]*RAD_TO_DEG);
+    printf(" | %f\r\n",mpu_data.dmp_TaitBryan[TB_YAW_Z]*RAD_TO_DEG);
 }
